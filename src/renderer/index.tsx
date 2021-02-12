10729082ce7ad276f263881620e12f4fb0440db5
Aaron.Olds <aaron.olds@gmail.com>
2021-02-12 14:25:29 -0500

    Added react-redux

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// const mainElement = document.createElement('div');
+// document.body.appendChild(mainElement);
+
+// const App = () => {
+//     return (<h1>        Hi from a world app            </h1>);
+// };
+
+// ReactDOM.render(<App />, mainElement);
+
+import * as React from "react";
+import { render } from "react-dom";
+import { createStore, applyMiddleware, Store } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import App from "./App";
+import reducer from "../store/reducer";
 
 const mainElement = document.createElement('div');
+mainElement.setAttribute("id", "root");
 document.body.appendChild(mainElement);
 
-const App = () => {
-    return (<h1>        Hi from a world app            </h1>);
-};
+const store: Store<ArticleState, ArticleAction> & {
+    dispatch: DispatchType;
+} = createStore(reducer, applyMiddleware(thunk));
 
-ReactDOM.render(<App />, mainElement);
+const rootElement = document.getElementById("root");
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , rootElement
+);
